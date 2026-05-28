363feeed70fe8e23828934c9cc2671d4e3c06d9b
propraetax <propraetax@gmail.com>
2026-05-28 11:45:55 +0700

    change directory paths

--- Program.cs
+++ Program.cs
@@ -5,13 +5,21 @@
         static void Main(string[] args)
         {
             var Organizer = new FileOrganizer();
+            Organizer.Start();
         }
     }
     class FileOrganizer
     {
         public void Start()
         {
+            string? srcPath = Console.ReadLine()?.Trim().Trim('"');
+            if (string.IsNullOrWhiteSpace(srcPath) ||!Directory.Exists(srcPath))
+            {
+                Console.WriteLine("Папка не найдена");
+                return;
 
+            }
+            var path = File.ReadLines(srcPath);
         }
         private void OrganizeFiles()
         {
